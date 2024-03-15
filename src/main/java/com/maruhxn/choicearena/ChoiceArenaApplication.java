d16f9b229c0ee124bce1540fddedf4aef940c951
maruhxn <maruhan1016@gmail.com>
2024-03-15 15:59:47 +0900

    Initial Commit:)

---
+++ src/main/java/com/maruhxn/choicearena/ChoiceArenaApplication.java
@@ -0,0 +1,13 @@
+package com.maruhxn.choicearena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoiceArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoiceArenaApplication.class, args);
+	}
+
+}
