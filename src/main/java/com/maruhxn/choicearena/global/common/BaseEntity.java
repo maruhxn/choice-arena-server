085f5963da5cab4bdf84da5a64c744785dacaca9
maruhxn <maruhan1016@gmail.com>
2024-03-15 16:19:40 +0900

    Feat: Add BaseEntity

---
+++ src/main/java/com/maruhxn/choicearena/global/common/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.maruhxn.choicearena.global.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
