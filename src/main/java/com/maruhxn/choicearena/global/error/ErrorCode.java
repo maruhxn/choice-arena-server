ba121449511788c5f117a3c02e017242d4d037c4
maruhxn <maruhan1016@gmail.com>
2024-03-15 16:10:11 +0900

    Feat: Add ErrorCode

---
+++ src/main/java/com/maruhxn/choicearena/global/error/ErrorCode.java
@@ -0,0 +1,30 @@
+package com.maruhxn.choicearena.global.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    /* BAD REQUEST 400 */
+    BAD_REQUEST("잘못된 접근입니다."),
+    VALIDATION_ERROR("올바르지 않은 입력입니다."),
+    PATH_ERROR("올바르지 않은 경로입니다."),
+
+    /* UNAUTHORIZED 401 */
+    UNAUTHORIZED("로그인이 필요한 서비스입니다."),
+
+    /* FORBIDDEN 403 */
+    FORBIDDEN("권한이 없습니다."),
+
+    /* NOT FOUND 404 */
+    NOT_FOUND_RESOURCE("요청하신 자원이 존재하지 않습니다."),
+
+    /* UNPROCESSABLE CONTENT 422 */
+    EXISTING_RESOURCE("이미 존재하는 리소스입니다."),
+
+    /* INTERNAL SERVER ERROR  500 */
+    INTERNAL_ERROR("서버 오류입니다.");
+
+    private final String message;
+}
