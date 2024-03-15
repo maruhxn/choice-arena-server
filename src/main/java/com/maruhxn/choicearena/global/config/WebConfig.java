d07484b1cc9b7240f46dea5fd609080e8d25c705
maruhxn <maruhan1016@gmail.com>
2024-03-15 16:05:38 +0900

    Feat: Add WebConfig, JpaConfig

---
+++ src/main/java/com/maruhxn/choicearena/global/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.maruhxn.choicearena.global.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+}
